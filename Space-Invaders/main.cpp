8c3b7577bf3d790677110510d682a3a1a616dca2
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2024-05-25 12:56:36 +0530

    Generated the x64 folder, added bin files and tested a single window.

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,12 @@
+#include <./SFML/Graphics.hpp>
 
 int main()
 {
+    // Defining the video mode dimensions
+    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
+
+    // Spawn a window object with dimensions and a title
+    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "Lazy SMFL Window");
+
     return 0;
 }
