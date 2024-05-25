666949857c9b4b99bc1c7efdfe9c69cf37a15d9e
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2024-05-25 13:03:34 +0530

    Created a basic loop and set the window to yellow

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -8,5 +8,25 @@ int main()
     // Spawn a window object with dimensions and a title
     sf::RenderWindow* window = new sf::RenderWindow(videoMode, "Lazy SMFL Window");
 
+    // Game loop
+    while (window->isOpen()) {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            // Check window closure
+            if (event.type == sf::Event::Closed)
+            {
+                window->close();
+            }
+
+            // Clear window
+            window->clear(sf::Color::Yellow);
+
+
+            // Display for drawn item
+            window->display();
+        }
+    }
+
     return 0;
 }
