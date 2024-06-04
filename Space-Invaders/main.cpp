9c7a9ebf1ee4428808183b150f715362ca6e656d
TheLazyIndianTechie <thelazyindiantechie@gmail.com>
2024-06-04 20:32:57 +0530

    Added a basic SFLM window and created an empty Player class;

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,39 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
 
-int main()
+class Player
 {
-    return 0;
+
+};
+
+int main() {
+
+// Define the video mode
+	VideoMode videoMode = VideoMode(800, 600);
+
+	// Create window object with dimensions
+	RenderWindow window(videoMode, "SFML Window");
+
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Check for clicks to close event
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		// Clear the window with Yellow
+		window.clear(Color::Color::Yellow);
+
+		window.display();
+	}
+
+	return 0;
 }
